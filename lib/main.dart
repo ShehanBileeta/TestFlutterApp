095f0efd19bc9426293881af644c348706ff244d
Vishal <vishal.ellalagoda@bileeta.com>
2025-08-15 11:58:00 +0530

    check cd test 2

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Login Form',
+      title: 'Simple Login Form Test',
       home: LoginPage(),
     );
   }
